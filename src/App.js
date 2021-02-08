6c34ca75a64da36fb35bcb38e881d6f530ce19d1
josephyim <jsph153@gmail.com>
2021-02-08 10:35:16 +0800

    Revert "revert "add api""
    This reverts commit e96db052ad7683fec90464bf8f8e34fc7536b5c5.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react'
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { listBlogs } from './graphql/queries';
+import { createBlog as createBlogMutation, deleteBlog as deleteBlogMutation } from './graphql/mutations';
+
+const initialFormState = { name: ''}
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  async function fetchBlogs() {
+    const apiData = await API.graphql({ query: listBlogs });
+    setBlogs(apiData.data.listBlogs.items);
+  }
+
+  async function createBlog() {
+    if (!formData.name) return;
+    await API.graphql({ query: createBlogMutation, variables: { input: formData } });
+    setBlogs([ ...blogs, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteBlog({ id }) {
+    const newBlogsArray = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogsArray);
+    await API.graphql({ query: deleteBlogMutation, variables: { input: { id } }});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello Natalie!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Blogs App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Blog name"
+        value={formData.name}
+      />
+      <button onClick={createBlog}>Create blog</button>
+      <div style={{marginBottom: 30}}>
+        {
+          blogs.map(blog => (
+            <div key={blog.id || blog.name}>
+              <h2>{blog.name}</h2>
+              <h2>{blog.id}</h2>
+              <button onClick={() => deleteBlog(blog)}>Delete blog</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   );
